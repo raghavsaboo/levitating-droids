8427b13a6599aac7e3f49a98d4aacc2416aace90
Raghav Saboo <raghavsaboo@users.noreply.github.com>
2020-01-04 21:08:57 -0500

    updated animation to be smoother

--- dancing-cubes/sketch.js
+++ dancing-cubes/sketch.js
@@ -3,16 +3,33 @@
 
 let angle = 0;
 let w = 40;
-let ma
+let framerate = 30;
+let ma;
 let maxD;
+let canvas
+var capturer = new CCapture({
+    format: 'png',
+    framerate: framerate,
+    name: 'dancing_cubes',
+    quality: 100,
+});
 
 function setup() {
-    createCanvas(500, 500, WEBGL);
+    let p5canvas = createCanvas(500, 500, WEBGL);
+    canvas = p5canvas.canvas;
     ma = atan(cos(QUARTER_PI));
     maxD = dist(1, 0, 150, 100);
+    capturer.start();
 }
 
 function draw() {
+    let secondsElapsed = frameCount / framerate;
+    if (secondsElapsed >= 10) {
+        capturer.stop();
+        capturer.save();
+        noLoop();
+        console.log(`Render time ${floor(millis() / 1000)} seconds`)
+    }
     background(210);
     ortho(-width, width, height, -height, 10, 1000);
     rotateX(-ma)
@@ -26,10 +43,13 @@ function draw() {
             let h = floor(map(sin(a), -1, 1, 50, 300));
             translate(x - width / 2, 0, z - height / 2);
             normalMaterial()
-            box(w, h, w, 50000, 50000);
+            box(w, h, w, 500000, 500000);
             pop()
         }
     }
 
     angle -= 0.05;
+
+    // Code for drawing the frame
+    capturer.capture(canvas)
 }
