7fd9d5b5171a89a2af4d8344b86b5efaadc09b75
Raghav Saboo <raghavsaboo@users.noreply.github.com>
2020-01-04 20:33:07 -0500

    updated animation to be smoother

--- dancing-cubes/sketch.js
+++ dancing-cubes/sketch.js
@@ -2,35 +2,34 @@
 // Reproduced by CodingTrain (Daniel Schiffman): https://youtu.be/H81Tdrmz2LA
 
 let angle = 0;
-let w = 30;
-let ma;
+let w = 40;
+let ma
 let maxD;
 
 function setup() {
-    createCanvas(400, 400, WEBGL);
+    createCanvas(500, 500, WEBGL);
     ma = atan(cos(QUARTER_PI));
-    maxD = dist(1, 0, 300, 300);
+    maxD = dist(1, 0, 150, 100);
 }
 
 function draw() {
-    background(200);
-    ortho(-width, width, height, -height, 10, 800);
+    background(210);
+    ortho(-width, width, height, -height, 10, 1000);
     rotateX(-ma)
     rotateY(QUARTER_PI);
-    ambientLight(255, 255, 250) 
     for (let z = 0; z < height; z += w) {
         for (let x = 0; x < width; x += w) {
             push();
             let d = dist(x, z, width / 2, height / 2);
-            let offset = map(d, 0, maxD, -PI, PI);
+            let offset = map(d, 0, maxD, -PI^2, PI);
             let a = angle + offset;
             let h = floor(map(sin(a), -1, 1, 50, 300));
             translate(x - width / 2, 0, z - height / 2);
             normalMaterial()
-            box(w, h, w);
+            box(w, h, w, 50000, 50000);
             pop()
         }
     }
 
-    angle -= 0.20;
+    angle -= 0.05;
 }
