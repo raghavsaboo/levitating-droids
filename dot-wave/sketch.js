b52f33f528e26e389727e3bae0af10b9ff313509
Raghav Saboo <saboo.raghav@gmail.com>
2020-03-09 00:13:55 -0400

    added dot wave animation

--- dot-wave/sketch.js
+++ dot-wave/sketch.js
@@ -2,6 +2,8 @@ let framerate = 60;
 let t = 0;
 let canvas
 let width = 640;
+let f = 5;
+
 var capturer = new CCapture({
     format: 'png',
     framerate: framerate,
@@ -11,23 +13,28 @@ var capturer = new CCapture({
 
 function setup(){
     let p5canvas = createCanvas(width, width);
-    strokeWeight(f=5)
     canvas = p5canvas.canvas;
+    strokeWeight(5);
+    background(51);
+    loop();
     capturer.start();
 }
 
 function draw(){
     let secondsElapsed = frameCount / framerate;
-    if (secondsElapsed >= 10) {
+    if (secondsElapsed >= 5) {
         capturer.stop();
         capturer.save();
         noLoop();
         console.log(`Render time ${floor(millis() / 1000)} seconds`);
     }
-    clear(t=map(sin(f+=0.01), -1, 1, 64, 200))
-    for (y=4; y<width; y+=8) {
-        for(x=4; x<width; x+=8) {
-            point(cos(r=noise(x/width, y/width - f) * t) * TAU + x, sin(r) * TAU + y)
+    strokeWeight(2);
+    background(51);
+    t=map(sin(f+=0.01), -1, 1, 64, 200);
+    for (y=-20; y<width+20; y+=5) {
+        for(x=-20; x<width+20; x+=5) {
+            stroke("white")
+            point(cos(r=noise(x/width, y/width - f) * t) * TAU + x, sin(r) * TAU + y);
         }
     }
     capturer.capture(canvas);
